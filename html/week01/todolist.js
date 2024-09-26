c71ec3eaa31fda4fd2622f59579b655772c2dcd3
seunghyeonKang <number42isuser@gmail.com>
2024-09-26 23:27:21 +0900

    [week01] todolist

---
+++ html/week01/todolist.js
@@ -0,0 +1,41 @@
+const todoForm = document.querySelector("form");
+const todoInput = document.querySelector("form input");
+const todoList = document.querySelector(".todo-container");
+const completedList = document.querySelector(".completed-container");
+
+todoForm.addEventListener("submit", (event) => {
+  event.preventDefault();
+  const todoText = todoInput.value.trim();
+
+  if (todoText !== "") {
+    addTodoItem(todoText);
+    todoInput.value = "";
+  }
+});
+
+function addTodoItem(todoText) {
+  const todoItem = createItem(todoText, "완료", handleComplete);
+  todoList.appendChild(todoItem);
+}
+
+function createItem(text, buttonText, buttonHandler) {
+  const item = document.createElement("li");
+  item.innerHTML = `
+        <p>${text}</p>
+        <button>${buttonText}</button>
+      `;
+  const button = item.querySelector("button");
+  button.addEventListener("click", () => buttonHandler(item));
+  return item;
+}
+
+function handleComplete(todoItem) {
+  const todoText = todoItem.querySelector("p").innerText;
+  const completedItem = createItem(todoText, "삭제", handleDelete);
+  completedList.appendChild(completedItem);
+  todoItem.remove();
+}
+
+function handleDelete(completedItem) {
+  completedItem.remove();
+}
